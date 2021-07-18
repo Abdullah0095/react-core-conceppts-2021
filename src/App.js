6cbdde98b904f7d3c833157e6b723167ae929e1e
Abdullah0095 <abdullah.web1995@gmail.com>
2021-07-18 09:21:19 +0600

    similar kaj

--- src/App.js
+++ src/App.js
@@ -5,11 +5,29 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Sakib Al Hasan</h1>
-        <h2>Run: {130-30}</h2>
-        <h3>age: {50-34}</h3>
+        <h2>Run: {130 - 30}</h2>
+        <h3>age: {50 - 34}</h3>
+        <Bangladesh></Bangladesh>
+        <Bangladesh></Bangladesh>
       </header>
     </div>
   );
+}
+
+function Bangladesh() {
+  const bdStyle = {
+    margin: '5px',
+    border: '2px dotted red',
+    borderRadius: '5px',
+    padding: '0px 15px'
+  }
+  return (
+    <div style={bdStyle}>
+      <h1>mashrafee bin mortuja</h1>
+      <p>player of the year</p>
+    </div>
+  )
+
 }
 
 export default App;
